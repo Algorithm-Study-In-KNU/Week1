35dcd89eec42b211765407dfe6c40bcf7f1af334
Seung_Woo <114336975+Dev-SSW@users.noreply.github.com>
2022-10-12 00:43:07 +0900

    Delete 폰켓몬.py

--- Hash/Dev-SSW/폰켓몬.py
+++
@@ -1,9 +0,0 @@
-def solution(nums):
-    answer = 0
-    maxleng = int(len(nums) / 2)
-    l = set(nums)
-    if len(l) <= maxleng:
-        answer = len(l)
-    else:
-        answer = maxleng
-    return answer
